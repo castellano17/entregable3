a7f2b5c5306cff2bcdd1fbff9cd23e1d2f32b75f
unknown <castellano17@gmail.com>
2023-04-05 17:47:23 -0600

    improvement in input

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import './styles/Pagination.css'
+import React from "react";
+import "./styles/Pagination.css";
 
-const Pagination = ({ numbersPage, setPage, location, RESIDENTS_PERPAGE, currentPage, setCurrentPage }) => {
-    return (
-        <ul className="pagination">
-            {
-                numbersPage(location, RESIDENTS_PERPAGE).map(numberPage => <li className={currentPage === numberPage ? 'selected' : ''} onClick={() => { setPage(numberPage); setCurrentPage(numberPage); }} key={numberPage}> {numberPage}</li>)
-            }
-        </ul>
-    )
-}
+const Pagination = ({
+  numbersPage,
+  setPage,
+  location,
+  RESIDENTS_PERPAGE,
+  currentPage,
+  setCurrentPage,
+}) => {
+  return (
+    <ul className="pagination">
+      {numbersPage(location, RESIDENTS_PERPAGE).map((numberPage) => (
+        <li
+          className={currentPage === numberPage ? "selected" : ""}
+          onClick={() => {
+            setPage(numberPage);
+            setCurrentPage(numberPage);
+          }}
+          key={numberPage}
+        >
+          {" "}
+          {numberPage}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-
-export default Pagination
+export default Pagination;
--- src/components/ResidentForm.jsx
+++ src/components/ResidentForm.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import './styles/ResidentForm.css'
+import React from "react";
+import "./styles/ResidentForm.css";
 
 const ResidentForm = ({ handleSubmit }) => {
-    return (
+  return (
+    <form className="header" onSubmit={handleSubmit}>
+      <div className="search">
+        <input id="idLocation" type="text" placeholder="Type a location id" />
+        <button>Search</button>
+      </div>
+    </form>
+  );
+};
 
-        <form className='header' onSubmit={handleSubmit}  >
-            <input id='idLocation' type="text" placeholder='Type a location id' />
-            <button>Search</button>
-        </form>
-    )
-}
-
-export default ResidentForm
+export default ResidentForm;
