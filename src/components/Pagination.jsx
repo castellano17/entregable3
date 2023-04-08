a30b18a3d4868b84fb5a58340a5f6296cc68c967
unknown <castellano17@gmail.com>
2023-04-07 19:44:51 -0600

    inprovement in numbering

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -9,9 +9,34 @@ const Pagination = ({
   currentPage,
   setCurrentPage,
 }) => {
+  const MAX_PAGES = 4;
+  const startPage = Math.max(currentPage - MAX_PAGES, 1);
+  const endPage = Math.min(
+    currentPage + MAX_PAGES,
+    numbersPage(location, RESIDENTS_PERPAGE).length
+  );
+
   return (
     <ul className="pagination">
-      {numbersPage(location, RESIDENTS_PERPAGE).map((numberPage) => (
+      {/* Botón "anterior" */}
+      {currentPage > 1 && (
+        <li
+          className="pagination__page"
+          onClick={() => {
+            setPage(currentPage - 1);
+            setCurrentPage(currentPage - 1);
+          }}
+          key="previous"
+        >
+          {"<"}
+        </li>
+      )}
+
+      {/* Páginas */}
+      {Array.from(
+        { length: endPage - startPage + 1 },
+        (_, i) => i + startPage
+      ).map((numberPage) => (
         <li
           className={`pagination__page ${
             currentPage === numberPage && "selected"
@@ -25,6 +50,20 @@ const Pagination = ({
           {numberPage}
         </li>
       ))}
+
+      {/* Botón "siguiente" */}
+      {currentPage < numbersPage(location, RESIDENTS_PERPAGE).length && (
+        <li
+          className="pagination__page"
+          onClick={() => {
+            setPage(currentPage + 1);
+            setCurrentPage(currentPage + 1);
+          }}
+          key="next"
+        >
+          {">"}
+        </li>
+      )}
     </ul>
   );
 };
